89316089a6605efc16450689c6d74ca7cc9dcf82
Sezgin UZUNÇELEBİ <infossyazilim@gmail.com>
2026-04-05 10:55:37 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-main.js
+fetch('components/navbar.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('navbar').innerHTML = data;
+  });
+
+fetch('components/footer.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  });
